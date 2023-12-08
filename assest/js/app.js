b1942765c1bd5154effb03850338098d7ad41fd7
vikram-jakhar <vikramjakhar144@gmail.com>
2023-12-08 12:02:18 +0530

    feedback update solve

--- assest/js/app.js
+++ assest/js/app.js
@@ -2,6 +2,7 @@ function openNav() {
   document.getElementById("navbar").classList.toggle("start-0");
   document.body.classList.toggle("overflow-hidden");
   document.querySelector(".menu").classList.toggle("cross")
+  document.getElementById("buler-bg").classList.toggle("nav-bg")
 }
 $('.slider-1').slick({
   dots: false,
@@ -70,46 +71,63 @@ $('.slider-1').slick({
 document.addEventListener("DOMContentLoaded", function () {
   const button = document.getElementById("back-to-top");
   window.onscroll = function () {
-      if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-          button.style.display = "block";
-      } else {
-          button.style.display = "none";
-      }
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
   };
   button.onclick = function () {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   };
 });
 
 $('.slider3').slick({
-    centerMode: true,
-    centerPadding: '240px',
-    slidesToShow: 1,
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          infinite: true,
-          slidesToShow: 1,
-          centerMode: true,
-          centerPadding: "200px",
-          dots:false,
-        }
-      },
-      {
-        breakpoint: 850,
-        settings: {
-          infinite: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          centerMode: false,
-          dots:false,
-        }
-      },
-    ]
+  centerMode: true,
+  centerPadding: '240px',
+  slidesToShow: 1,
+  dots: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        infinite: true,
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: "200px",
+        dots: false,
+      }
+    },
+    {
+      breakpoint: 850,
+      settings: {
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+        dots: false,
+      }
+    },
+  ]
+});
+// accordian
+let box = document.querySelectorAll(".box");
+box.forEach((row) => {
+  row.addEventListener("click", () => {
+    const x = document.querySelector(".active");
+    row.classList.toggle("active");
+    x && x.classList.remove("active");
+
+    if (!isOpen) {
+      // Smoothly open the clicked item
+      content.style.maxHeight = content.scrollHeight + "px";
+    } else {
+      // Close the clicked item
+      content.style.maxHeight = null;
+    }
   });
+});
